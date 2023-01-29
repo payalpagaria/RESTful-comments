a9fdf8cf8eb2a081dc658d899b4684c12ded7c68
payalpagaria <pagariapayal67@gmail.com>
2023-01-29 23:15:39 +0530

    routes addes for geting id

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 import  Express  from "express";
 import bodyParser from "body-parser";
+import { v4 } from "uuid";
+
 const PORT=3000;
 const app=Express();
-let commentList=[]
+const commentList=[]
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
     res.render('all',{Day:getdate(),comments:commentList});
 })
 app.post('/',(req,res)=>{
+    let com_id=v4();
     let name=req.body.username;
     let newComment=req.body.comment;
-    commentList.push({username:name,comment:newComment});
+    commentList.push({id:com_id,username:name,comment:newComment});
     res.redirect('/')
+})
+app.get("/:id",(req,res)=>{
+    const comment_id=req.params.id;
+    res.render('show',{prid:comment_id,comments:commentList});
 })
 function getdate(){
     let date=new Date();
